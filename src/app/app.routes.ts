91f099ee7ff428d9b138d610d67b2022de6f7dd8
JuanDChaves <juandavidchaves@protonmail.com>
2026-04-08 01:30:41 -0300

    Estructura inicial del menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { Bienvenida } from './bienvenida/bienvenida';
+import { Login } from './login/login';
+import { Registro } from './registro/registro';
+import { SobreMi } from './sobre-mi/sobre-mi';
+import { Error } from './error/error';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "bienvenida",
+        component: Bienvenida
+    }, 
+    {
+        path: "login",
+        component: Login
+    }, 
+    {
+        path: "registro",
+        component: Registro
+    }, 
+    {
+        path: "sobre-mi",
+        component: SobreMi
+    }, 
+    {
+        path: "",
+        redirectTo: "bienvenida",
+        pathMatch: "full"
+    }, 
+    {
+        path: "**",
+        component: Error
+    }
+];
